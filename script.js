3a71d13b40788f0b3113a42100b46c1b130a1c4a
Ankitha Bangar <ankitha.bangar@st.ovgu.de>
2024-03-26 20:08:23 +0100

    Added more functionalities

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-let operand1;
-let operator;
-let operand2;
+let operand1 = 0;
+let operator = "";
+let operand2 = 0;
+let displayValue = "";
+let result = 0;
+
 const display = document.querySelector(".display");
 const numbers = document.querySelectorAll(".number");
-let displayValue = "";
+const equals = document.querySelector(".equals");
+const operators = document.querySelectorAll(".operator");
+const clear = document.querySelector(".clear");
+const point = document.querySelector(".point");
+//const sqrt = document.querySelector(".sqrt");
+//const modulus = document.querySelector(".modulus");
+
+point.addEventListener("click", () => {
+  updateDisplay(".");
+});
 
 numbers.forEach((number) =>
   number.addEventListener("click", function () {
@@ -11,37 +23,85 @@ numbers.forEach((number) =>
   })
 );
 
+operators.forEach((operatorType) => {
+  operatorType.addEventListener("click", () => {
+    if (operand1 !== 0 && operator !== "" && operand2 !== 0) {
+      clearDisplay();
+      operate(operand1, operator, operand2);
+      console.log("Operand 1 when operator is clicked= ", operand1);
+      console.log("The operator when operator is clicked= ", operator);
+      operand2 = 0;
+    }
+    operator = operatorType.textContent;
+    console.log("Operand 1 when operator is clicked= ", operand1);
+    console.log("The operator when operator is clicked= ", operator);
+    clearDisplay();
+  });
+});
+
 function updateDisplay(number) {
   displayValue += number;
   display.textContent = displayValue;
-  console.log(display.textContent);
+  if (operator !== "") {
+    //So only during the first time when I enter the number it will be
+    //considered as operand1 and the rest of the time it will be operand2
+    operand2 = Number(displayValue);
+    console.log("operand2= ", operand2);
+  } else {
+    operand1 = Number(displayValue);
+    console.log("operand1= ", operand1);
+  }
+}
+function round(num) {
+  return Math.round(num * 100) / 100;
+}
+
+function squareRoot(num) {
+  result = Math.sqrt(num);
+  updateDisplay(result);
+  operand1 = result;
+  operator = "";
 }
 
 function add(num1, num2) {
-  let result = 0;
-  result = parseInt(num1 + num2);
+  result = round(num1 + num2);
   console.log("add= ", result);
+  updateDisplay(result);
+  operand1 = result;
+  operator = "";
   return result;
 }
 
 function subtract(num1, num2) {
-  let result = 0;
-  result = parseInt(num1 - num2);
+  result = round(num1 - num2);
   console.log("subtract= ", result);
+  updateDisplay(result);
+  operand1 = result;
+  operator = "";
   return result;
 }
 
 function multiply(num1, num2) {
-  let result = 0;
-  result = parseInt(num1 * num2);
+  result = round(num1 * num2);
   console.log("multiply", result);
+  updateDisplay(result);
+  operand1 = result;
+  operator = "";
   return result;
 }
 
 function divide(num1, num2) {
-  let result = 0;
-  result = parseInt(num1 / num2);
+  if (num2 === 0) {
+    display.textContent = "LMAO";
+    console.log(display.textContent);
+    num2 = 1;
+  }
+  console.log(num2);
+  result = round(num1 / num2);
   console.log("divide", result);
+  updateDisplay(result);
+  operand1 = result;
+  operator = "";
   return result;
 }
 
@@ -59,7 +119,33 @@ function operate(operand1, operator, operand2) {
     case "/":
       divide(operand1, operand2);
       break;
+    case "√":
+      squareRoot(operand1);
+      break;
   }
 }
+function clearDisplay() {
+  displayValue = "";
+}
 
-function showNumberDisplay() {}
+function clearValues() {
+  operand1 = 0;
+  operand2 = 0;
+  operator = "";
+  displayValue = "";
+  display.textContent = displayValue;
+}
+
+equals.addEventListener("click", () => {
+  if (operand1 !== 0 && operator !== "" && operand2 !== 0) {
+    clearDisplay();
+    operate(operand1, operator, operand2);
+    console.log("Operand 1 when operator is clicked= ", operand1);
+    console.log("Operand2 when = is clicked= ", operand2);
+    console.log("The operator when operator is clicked= ", operator);
+    operand2 = 0;
+    clearDisplay();
+  }
+});
+
+clear.addEventListener("click", clearValues);
